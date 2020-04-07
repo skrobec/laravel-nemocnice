ca2e76467ed33a02d3e2d53e6a14e3793bbe84cf
Adam Jiruška <adam.jiruska@bepositive.cz>
2020-04-07 19:43:08 +0200

    fix

--- database/seeds/DoctorsTableSeeder.php
+++ database/seeds/DoctorsTableSeeder.php
@@ -11,19 +11,16 @@ class DoctorsTableSeeder extends Seeder
         DB::table('doctors')->delete();
 
         DB::table('doctors')->insert([
-            'user_id' => 1,
             'entry_date' => Carbon::now()->format('Y-m-d H:i:s'),
             'termination_date' => Carbon::now()->format('Y-m-d H:i:s'),
         ]);
 
         DB::table('doctors')->insert([
-            'user_id' => 2,
             'entry_date' => Carbon::now()->format('Y-m-d H:i:s'),
             'termination_date' => Carbon::now()->format('Y-m-d H:i:s'),
         ]);
 
         DB::table('doctors')->insert([
-            'user_id' => 3,
             'entry_date' => Carbon::now()->format('Y-m-d H:i:s'),
             'termination_date' => Carbon::now()->format('Y-m-d H:i:s'),
         ]);
--- database/seeds/NursesTableSeeder.php
+++ database/seeds/NursesTableSeeder.php
@@ -11,21 +11,18 @@ class NursesTableSeeder extends Seeder
         DB::table('nurses')->delete();
 
         DB::table('nurses')->insert([
-            'user_id' => 4,
             'section_id' => 1,
             'entry_date' => Carbon::now()->format('Y-m-d H:i:s'),
             'termination_date' => Carbon::now()->format('Y-m-d H:i:s'),
         ]);
 
         DB::table('nurses')->insert([
-            'user_id' => 5,
             'section_id' => 1,
             'entry_date' => Carbon::now()->format('Y-m-d H:i:s'),
             'termination_date' => Carbon::now()->format('Y-m-d H:i:s'),
         ]);
 
         DB::table('nurses')->insert([
-            'user_id' => 6,
             'section_id' => 1,
             'entry_date' => Carbon::now()->format('Y-m-d H:i:s'),
             'termination_date' => Carbon::now()->format('Y-m-d H:i:s'),
